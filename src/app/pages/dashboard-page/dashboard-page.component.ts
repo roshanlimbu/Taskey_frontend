6e76317b4910d634a53dad45e64e358a882cdb56
roshanlimbu <roshanlimbu22@outlook.com>
2025-03-28 22:51:02 +0545

    dashboard page basic

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [],
   templateUrl: './dashboard-page.component.html',
-  styleUrl: './dashboard-page.component.scss'
+  styleUrl: './dashboard-page.component.scss',
 })
 export class DashboardPageComponent {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService,
+  ) {}
 
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const token = params['token'];
+      if (token) {
+        // localStorage.setItem('authToken', token);
+        this.authService.setToken(token);
+        // this.router.navigate(['/dashboard']);
+      }
+    });
+  }
+
+  logout() {
+    // For now, just redirect back to login (add backend logout later)
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
