9681628ff4fdb731deb6d80354fcd9e2e150e5bb
roshanlimbu <topazkayle@gmail.com>
2025-04-24 22:11:47 +0545

    feat: implement GitHub login functionality using AuthService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
 @Component({
   selector: 'app-root',
   imports: [CommonModule, FormsModule, RouterOutlet],
@@ -10,9 +12,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   loginWithGitHub() {
-    window.location.href = 'http://localhost:8000/auth/github';
+    this.authService.loginWithGithub();
   }
 }
