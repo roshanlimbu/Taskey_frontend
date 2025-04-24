81be12f36d911e81f34213fe99d76440f05033bc
roshanlimbu <topazkayle@gmail.com>
2025-04-25 00:47:15 +0545

    refactor: update bootstrapApplication to include providers for routing and HTTP client with XSRF configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.module';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withXsrfConfiguration,
+} from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+  ],
+}).catch((err) => console.error(err));
