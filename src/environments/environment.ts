09c73f19275e25c5cdb3a4e271af4dd33a0388ef
roshanlimbu <topazkayle@gmail.com>
2025-05-05 18:48:56 +0545

    refactor: remove environment configuration file as it is no longer needed

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  githubClientId: 'Iv23liVS3iPc1qwfXQsT',
-  apiUrl: 'http://localhost:8000/api',
-  redirectUri: 'http://localhost:4200/auth/callback',
-};
