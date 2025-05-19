834e93782ef14ec12f5a5fb653f29385d90eafb7
roshanlimbu <topazkayle@gmail.com>
2025-05-19 22:52:40 +0545

    feat: lazyload the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,30 +6,42 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () =>
       import('./pages/login-page/login-page.component').then(
-        (m) => m.LoginPageComponent
+        (m) => m.LoginPageComponent,
       ),
   },
   {
     path: 'login-callback',
     loadComponent: () =>
       import('./pages/auth-callback/auth-callback.component').then(
-        (m) => m.AuthCallbackComponent
+        (m) => m.AuthCallbackComponent,
       ),
   },
   {
     path: 'error',
     loadComponent: () =>
       import('./components/error/error.component').then(
-        (m) => m.ErrorComponent
+        (m) => m.ErrorComponent,
       ),
   },
   {
     path: 'dashboard',
     loadComponent: () =>
       import('./pages/dashboard-page/dashboard-page.component').then(
-        (m) => m.DashboardPageComponent
+        (m) => m.DashboardPageComponent,
       ),
   },
+
+  {
+    path: 'user',
+    loadChildren: () => import('./routes/user.routes').then((m) => m.routes),
+  },
+
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./routes/project-admin.routes').then((m) => m.routes),
+  },
+
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
 ];
