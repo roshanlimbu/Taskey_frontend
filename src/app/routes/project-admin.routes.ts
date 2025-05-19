6e8f2e97f29d2b9df982d0f96a5b1edae68b3464
roshanlimbu <topazkayle@gmail.com>
2025-05-19 22:52:32 +0545

    chore: basic routes

---
+++ src/app/routes/project-admin.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProjectAdminDashboardComponent } from '../pages/project-admin/project-admin-dashboard/project-admin-dashboard.component';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: ProjectAdminDashboardComponent,
+  },
+];
