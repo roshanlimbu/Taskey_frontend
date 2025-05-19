0758992b7ea68e970b77cad2f35f9a30b3d458f7
roshanlimbu <topazkayle@gmail.com>
2025-05-19 22:52:16 +0545

    feat: project-admin dash

---
+++ src/app/pages/project-admin/project-admin-dashboard/project-admin-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-admin-dashboard',
+  imports: [],
+  templateUrl: './project-admin-dashboard.component.html',
+  styleUrl: './project-admin-dashboard.component.scss',
+})
+export class ProjectAdminDashboardComponent {
+  ngONInit() {
+    console.log('Project Admin Dashboard Component Initialized');
+  }
+}
