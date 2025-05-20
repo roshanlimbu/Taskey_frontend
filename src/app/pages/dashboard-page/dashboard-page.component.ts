df9dfd51180b745b44213b8ae895c2101d978f63
roshanlimbu <topazkayle@gmail.com>
2025-05-20 12:51:00 +0545

    feat: add project context menu and edit functionality in dashboard

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
+import { HostListener } from '@angular/core';
 
 interface Project {
   id: number;
@@ -32,11 +33,20 @@ export class DashboardPageComponent implements OnInit {
 
   projects: Project[] = [];
 
+  showContextMenu = false;
+  contextMenuIndex: number | null = null;
+  contextMenuPosition = { x: 0, y: 0 };
+
+  showEditProjectForm = false;
+  isSubmittingEdit = false;
+  editProjectForm: FormGroup;
+  editingProjectId: number | null = null;
+
   constructor(
     private router: Router,
     private authService: AuthService,
     private fb: FormBuilder,
-    private apiService: ApiService,
+    private apiService: ApiService
   ) {
     this.repoForm = this.fb.group({
       name: ['', Validators.required],
@@ -44,6 +54,10 @@ export class DashboardPageComponent implements OnInit {
       private: [true],
     });
     this.projectForm = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+    });
+    this.editProjectForm = this.fb.group({
       name: ['', Validators.required],
       description: [''],
     });
@@ -88,6 +102,68 @@ export class DashboardPageComponent implements OnInit {
       error: (err) => {
         this.isSubmitting = false;
         console.error('Error creating project:', err);
+      },
+    });
+  }
+
+  openContextMenu(event: MouseEvent, index: number) {
+    event.stopPropagation();
+    this.showContextMenu = true;
+    this.contextMenuIndex = index;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (this.showContextMenu) {
+      this.showContextMenu = false;
+      this.contextMenuIndex = null;
+    }
+  }
+
+  editProject(project: any) {
+    this.showContextMenu = false;
+    this.showEditProjectForm = true;
+    this.editingProjectId = project.id;
+    this.editProjectForm.patchValue({
+      name: project.name,
+      description: project.description,
+    });
+  }
+
+  closeEditProjectForm() {
+    this.showEditProjectForm = false;
+    this.editingProjectId = null;
+  }
+
+  submitEditProject() {
+    if (this.editProjectForm.invalid || !this.editingProjectId) return;
+    this.isSubmittingEdit = true;
+    this.apiService
+      .put(
+        `sadmin/projects/${this.editingProjectId}`,
+        this.editProjectForm.value
+      )
+      .subscribe({
+        next: (res: any) => {
+          this.isSubmittingEdit = false;
+          this.closeEditProjectForm();
+          this.ngOnInit();
+        },
+        error: (err) => {
+          this.isSubmittingEdit = false;
+          console.error('Error editing project:', err);
+        },
+      });
+  }
+
+  deleteProject(project: any) {
+    this.showContextMenu = false;
+    this.apiService.delete(`sadmin/projects/${project}`).subscribe({
+      next: (res: any) => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.error('Error deleting project:', err);
       },
     });
   }
