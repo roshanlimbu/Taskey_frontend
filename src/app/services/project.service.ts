b4421c3418ab9ad118acaa36920c551df1b9f913
roshanlimbu <topazkayle@gmail.com>
2025-05-21 23:45:17 +0545

    feat: add getProjectDetails method to ProjectService for fetching project details

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -7,12 +7,13 @@ import * as jwt from 'jsonwebtoken';
   providedIn: 'root',
 })
 export class ProjectService {
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService,
-  ) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   createProject(data: { name: string; description?: string }) {
     return this.http.post('sadmin/projects', data);
+  }
+
+  getProjectDetails(id: number) {
+    return this.http.get(`sadmin/projects/${id}`);
   }
 }
