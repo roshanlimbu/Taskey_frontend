92103b9fd858a12882006a93d5e3c0e29400e70c
roshanlimbu <topazkayle@gmail.com>
2025-05-25 13:18:25 +0545

    refactor: remove user-dashboard component and associated files

--- src/app/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserDashboardComponent } from '../pages/user/user-dashboard/user-dashboard.component';
-
-export const routes: Routes = [
-  {
-    path: 'dashboard',
-    component: UserDashboardComponent,
-  },
-];
