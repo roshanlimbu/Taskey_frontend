226695a120cb7eb71b5c18c583ea55b9f667a90c
roshanlimbu <topazkayle@gmail.com>
2025-05-25 09:23:00 +0545

    feat: add profile route and corresponding lazy-loaded module for user profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,28 +6,28 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () =>
       import('./pages/login-page/login-page.component').then(
-        (m) => m.LoginPageComponent
+        (m) => m.LoginPageComponent,
       ),
   },
   {
     path: 'login-callback',
     loadComponent: () =>
       import('./pages/auth-callback/auth-callback.component').then(
-        (m) => m.AuthCallbackComponent
+        (m) => m.AuthCallbackComponent,
       ),
   },
   {
     path: 'error',
     loadComponent: () =>
       import('./components/error/error.component').then(
-        (m) => m.ErrorComponent
+        (m) => m.ErrorComponent,
       ),
   },
   {
     path: 'dashboard',
     loadComponent: () =>
       import('./pages/dashboard-page/dashboard-page.component').then(
-        (m) => m.DashboardPageComponent
+        (m) => m.DashboardPageComponent,
       ),
   },
 
@@ -46,8 +46,12 @@ export const routes: Routes = [
     path: 'project/:id',
     loadComponent: () =>
       import('./components/project/project.component').then(
-        (m) => m.ProjectComponent
+        (m) => m.ProjectComponent,
       ),
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./routes/profile.routes').then((m) => m.routes),
   },
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
---
+++ src/app/routes/profile.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProfileComponent } from '../components/profile/profile.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent,
+  },
+];
