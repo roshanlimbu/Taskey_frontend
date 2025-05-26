03ad032e53b882da9daedeeed8c3b69992469450
roshanlimbu <topazkayle@gmail.com>
2025-05-26 19:27:02 +0545

    feat: enhance user dashboard with profile dropdown, search, and notification icons

--- src/app/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-dashboard.component.html',
-  styleUrl: './user-dashboard.component.scss'
+  styleUrl: './user-dashboard.component.scss',
 })
-export class UserDashboardComponent {
+export class UserDashboardComponent implements OnInit {
+  user: any;
+  showProfileDropdown = false;
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  showProfile() {
+    this.showProfileDropdown = false;
+    this.router.navigate(['/profile']);
+  }
+
+  logout() {
+    // Implement logout logic here
+    localStorage.removeItem('user');
+    window.location.href = '/';
+  }
 }
