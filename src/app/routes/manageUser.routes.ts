33911d37ec06bf569faf4386a36a9657587d58ed
roshanlimbu <topazkayle@gmail.com>
2025-06-17 23:15:37 +0545

    new routes

---
+++ src/app/routes/manageUser.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { UserManageComponent } from '../components/user-manage/user-manage.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: UserManageComponent,
+  },
+];
