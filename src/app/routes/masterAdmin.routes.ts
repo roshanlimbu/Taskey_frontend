c18732f32507f6eef48b0b29570ad54ed46b3e06
Roshan Limbu <birenlimbu415@gmail.com>
2025-07-04 22:47:05 +0545

    feat: add routing for SuperAdminDashboard component

---
+++ src/app/routes/masterAdmin.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import(
+        '../pages/super-admin-dashboard/super-admin-dashboard.component'
+      ).then((m) => m.SuperAdminDashboardComponent),
+  },
+];
